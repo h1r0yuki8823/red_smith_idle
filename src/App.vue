ec347a275c3e0e8cc5088e990dff1578a1b8ceab
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2019-03-26 12:55:30 +0900

    add toppage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <Top/>
   </div>
 </template>
 
 <script>
 import Header from './components/global/header'
+import Top from './components/pages/top'
 
 export default {
 
   components: {
-    'app-header':Header
+    'app-header':Header,
+    'Top':Top
   }
 }
 </script>
---
+++ src/components/pages/top.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="top-content wrapper">
+      <p class="subLabel1">MEN'S IDLE UNIT</p>
+      <h2 class="mainLabel">RED SMITH</h2>
+      <p class="subLabel2">メンバー募集中</p>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
+
+.top-content{
+  font-family: 'Josefin Sans', sans-serif;
+}
+</style>
+
