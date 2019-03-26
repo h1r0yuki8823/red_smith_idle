0894a5663f3fbb0c3a8030293e96b8708eb86309
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2019-03-26 12:44:44 +0900

    add header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from './components/global/header'
+
+export default {
+
+  components: {
+    'app-header':Header
+  }
+}
+</script>
+
+<style>
+html{
+  font-size: 100%;
+}
+</style>
---
+++ src/components/global/header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="page-header wrapper">
+    <h1>Red Smith</h1>
+    <nav>
+      <ul class="main-nav">
+        <li>About</li>
+        <li>Member</li>
+        <li>Info</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Merriweather');
+
+.page-header{
+  font-family: 'Merriweather', serif; 
+  display:flex;
+  justify-content: space-between;
+}
+
+.main-nav{
+  display: flex;
+  font-size: 1.25rem;
+  text-transform: uppercase;
+  margin-top: 34px;
+  list-style: none;
+}
+
+.main-nav li{
+  margin-left: 36px;
+  text-decoration: none;
+}
+
+.wrapper{
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 4%;
+}
+
+</style>
